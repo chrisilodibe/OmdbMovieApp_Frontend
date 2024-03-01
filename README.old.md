d4f04c6dd9b77f7ee28c8bb16ece35da5a8d1397
Onyedika Ilodibe <ilodibeonyedikachisom@gmail.com>
2024-03-01 22:32:59 +0100

    frontend

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# OmdbMovieApp_Frontend
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import OmdbMovieApp from "./OmdbMovieApp";
+
+const App = () => {
+  return (
+    <>
+      <OmdbMovieApp />
+    </>
+  );
+};
+
+export default App;
